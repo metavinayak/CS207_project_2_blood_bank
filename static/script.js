3d0c819caac86943445c541129f2a08282ded332
Vinayak Sachan <vinayakasachan@gmail.com>
2021-11-08 01:42:39 +0530

    Console issues fixed,toggle added for register,login and using current date by default in search bar

--- static/script.js
+++ static/script.js
@@ -1,14 +1,15 @@
-
 document.querySelector("#show-login").addEventListener("click",function(){
-    document.querySelector(".loginpage").classList.add("active");
+    document.querySelector(".loginpage").classList.toggle("active");
   });
   document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".loginpage").classList.remove("active");
   });
 
 document.querySelector('#show-register').addEventListener("click",function() {
-    document.querySelector(".registerpage").classList.add("active");
+    document.querySelector(".registerpage").classList.toggle("active");
 });
 document.querySelector(".registerpage .close-btn").addEventListener("click",function(){
     document.querySelector(".registerpage").classList.remove("active");
   });
+
+
