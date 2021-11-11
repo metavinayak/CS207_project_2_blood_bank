30cb6e67778a66ac2fb7a483ba039e0a774cf6c7
Sukhija-Aniket <b20081@students.iitmandi.ac.in>
2021-11-11 19:17:01 +0530

    login/register fixed

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
 document.querySelector("#show-login").addEventListener("click",function(){
     document.querySelector(".loginpage").classList.toggle("active");
+    if(document.querySelector(".registerpage").classList.contains("active"))
+    document.querySelector(".registerpage").classList.remove("active");
   });
   document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".loginpage").classList.remove("active");
@@ -7,6 +9,8 @@ document.querySelector("#show-login").addEventListener("click",function(){
 
 document.querySelector('#show-register').addEventListener("click",function() {
     document.querySelector(".registerpage").classList.toggle("active");
+    if(document.querySelector(".loginpage").classList.contains("active"))
+    document.querySelector(".loginpage").classList.remove("active");
 });
 document.querySelector(".registerpage .close-btn").addEventListener("click",function(){
     document.querySelector(".registerpage").classList.remove("active");
